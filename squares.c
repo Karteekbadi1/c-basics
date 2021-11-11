5d208d82974406314319ce76eb5970f184e87a42
Karteekbadi1 <93382679+Karteekbadi1@users.noreply.github.com>
2021-11-11 12:35:42 +0530

    Add files via upload

---
+++ squares.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+   int s, a, p;
+   printf("enter one number");
+   scanf("%d", &s);
+   a = s*s;
+   p = 4*s;
+   printf("area of square is %d\n", a);
+   printf("perimeter of square is %d",p);
+}
